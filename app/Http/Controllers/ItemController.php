91967f09703c48f7651ef3624d015c22b323f1f0
rommel dalisay <rommelmdalisay@gmail.com>
2023-09-25 11:55:27 +0800

    authcontroller

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -16,7 +16,7 @@ class ItemController extends Controller
     public function index()
     {
         $items = Item::all();
-        return response()->json($items);
+        return response()->json(["data" => $items]);
     }
 
     /**
